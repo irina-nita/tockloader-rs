1e65d7f0038de180206d36dd4b84308b028ad5a7
Micu Ana <ana.micu@oxidos.io>
2024-07-25 15:06:03 +0300

    updated list for more than one app

--- tockloader/src/main.rs
+++ tockloader/src/main.rs
@@ -8,16 +8,19 @@ mod interfaces;
 
 use cli::make_cli;
 use errors::TockloaderError;
-<<<<<<< HEAD
-=======
+
 use interfaces::{build_interface, traits::*};
 use tokio::time::sleep;
 use probe_rs::probe::list::Lister;
 use probe_rs::{Permissions, MemoryInterface};
 use tbf_parser::parse::*;
+use tbf_parser::types::*;
 
->>>>>>> 717bc32 (tried list implementation)
+
 use tock_process_console;
+
+// use tock_process_console;
+
 
 #[tokio::main]
 async fn main() -> Result<(), TockloaderError> {
@@ -74,27 +77,58 @@ async fn list_probes() -> Result<(), TockloaderError> {
 
     let mut core = session.core(0).unwrap();
 
-    // Read a block of 200 8-bit words
-    let mut buff = vec![0u8; 200];
-    core.read(0x0004_0000, &mut buff).unwrap();
-    println!("Data: {:?}\n", buff);
+    let mut address = 0x0004_0000;
+    // Jump through the linked list of apps
+    loop {
+            // Read a block of 200 8-bit words
+            let mut buff = vec![0u8; 200];
+            let mut buff_not_valid = false;
+            core.read(address, &mut buff).unwrap_or_else(|err|{
+                println!("Error is {}", err);
+                buff_not_valid = true;
+            });
+            
+            if buff_not_valid {
+                break;
+            }
 
-    let (ver, header_len, whole_len) = parse_tbf_header_lengths(&buff[0..8].try_into().unwrap())
-        .ok()
-        .unwrap();
+            let (ver, header_len, whole_len) = match parse_tbf_header_lengths(&buff[0..8].try_into().unwrap()) {
+                Ok((ver, header_len, whole_len)) => {
+                    println!("Version: {:?}\n", ver);
+                    println!("Header length: {:?}\n", header_len);
+                    println!("Whole length: {:?}\n", whole_len);
+                    (ver, header_len, whole_len)
+                },
+                _ => {
+                    (0, 0, 0)
+                }
+            };
 
-    println!("Version: {:?}\n", ver);
-    println!("Header length: {:?}\n", header_len);
-    println!("Whole length: {:?}\n", whole_len);
+            if header_len == 0 {
+                break;
+            }
 
-    let header = parse_tbf_header(&buff[0..header_len as usize], ver).unwrap();
-
-    println!("Enabled: {:?}\n", header.enabled());
-    println!("Minimum App Ram Size: {:?}\n", header.get_minimum_app_ram_size());
-    println!("Init function offset: {:?}\n", header.get_init_function_offset());
-    println!("Protected size: {:?}\n", header.get_protected_size());
-    println!("Package name: {:?}\n", header.get_package_name().unwrap());
-    println!("Kernel version: {:?}\n", header.get_kernel_version().unwrap());
-
+            let header = parse_tbf_header(&buff[0..header_len as usize], ver);
+            match header {
+                Ok(header) => {
+                    println!("Enabled: {:?}\n", header.enabled());
+                    println!("Minimum App Ram Size: {:?}\n", header.get_minimum_app_ram_size());
+                    println!("Init function offset: {:?}\n", header.get_init_function_offset());
+                    println!("Protected size: {:?}\n", header.get_protected_size());
+                    println!("Package name: {:?}\n", header.get_package_name().unwrap_or_default());
+                    println!("Kernel version: {:?}\n", header.get_kernel_version().unwrap_or_default());
+                },
+                Err(TbfParseError::ChecksumMismatch(provided_checksum, calculated_checksum)) => {
+                    println!("Checksum mismatch: provided = {}, calculated = {}", provided_checksum, calculated_checksum);
+                    break;
+                },
+                Err(e) => {
+                    println!("Failed to parse TBF header: {:?}", e);
+                    break;
+                }
+            }
+            address += whole_len as u64;
+    }
+    
     Ok(())
 }
