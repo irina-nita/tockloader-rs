1d75f1ca084a965d9f065fa11dae48d6fb7adb33
Negrila Rares <rares_negrila@yahoo.com>
2024-08-01 11:47:56 +0300

    moved necessary code to tockloader-lib

--- tockloader-lib/src/lib.rs
+++ tockloader-lib/src/lib.rs
@@ -1,14 +1,111 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod board_settings;
+mod errors;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use board_settings::BoardSettings;
+use clap::ArgMatches;
+use errors::TockloaderError;
+use inquire::Select;
+use probe_rs::probe::list::Lister;
+use probe_rs::{MemoryInterface, Permissions};
+use tbf_parser::parse::*;
+use tbf_parser::types::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub async fn list_probes(sub_matches: &ArgMatches) -> Result<(), TockloaderError> {
+    let lister = Lister::new();
+    let probes = lister.list_all();
+    println!("Probes: {:?}\n", probes);
+
+    let ans = Select::new("Which probe do you want to use?", probes).prompt();
+
+    match ans {
+        Ok(choice) => {
+            let probe = choice.open().unwrap();
+
+            let chip = sub_matches.get_one::<String>("chip").unwrap();
+            let board = sub_matches.get_one::<String>("board").unwrap();
+
+            let board_settings = BoardSettings::new(board.clone(), chip.clone());
+
+            let mut session = probe
+                .attach(board_settings.chip, Permissions::default())
+                .unwrap();
+
+            println!("Session target: {:?}\n", session.target());
+            println!("Session interfaces: {:?}\n", session.architecture());
+            println!("Session core: {:?}\n", session.list_cores());
+
+            let core_index = sub_matches.get_one::<usize>("core").unwrap();
+
+            let mut core = session.core(*core_index).unwrap();
+
+            let mut address = board_settings.start_address;
+
+            // Jump through the linked list of apps
+            loop {
+                // Read a block of 200 8-bit words
+                let mut buff = vec![0u8; 200];
+                match core.read(address, &mut buff) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error reading memory: {:?}", e);
+                        break;
+                    }
+                }
+
+                let (ver, header_len, whole_len) =
+                    match parse_tbf_header_lengths(&buff[0..8].try_into().unwrap()) {
+                        Ok((ver, header_len, whole_len)) if header_len != 0 => {
+                            println!("Version: {:?}\n", ver);
+                            println!("Header length: {:?}\n", header_len);
+                            println!("Whole length: {:?}\n", whole_len);
+                            (ver, header_len, whole_len)
+                        }
+                        _ => break, // No more apps
+                    };
+
+                let header = parse_tbf_header(&buff[0..header_len as usize], ver);
+                match header {
+                    Ok(header) => {
+                        println!("Enabled: {:?}\n", header.enabled());
+                        println!(
+                            "Minimum App Ram Size: {:?}\n",
+                            header.get_minimum_app_ram_size()
+                        );
+                        println!(
+                            "Init function offset: {:?}\n",
+                            header.get_init_function_offset()
+                        );
+                        println!("Protected size: {:?}\n", header.get_protected_size());
+                        println!(
+                            "Package name: {:?}\n",
+                            header.get_package_name().unwrap_or_default()
+                        );
+                        println!(
+                            "Kernel version: {:?}\n",
+                            header.get_kernel_version().unwrap_or_default()
+                        );
+                    }
+                    // TODO(MicuAna): refactor when reworking errors
+                    Err(TbfParseError::ChecksumMismatch(
+                        provided_checksum,
+                        calculated_checksum,
+                    )) => {
+                        println!(
+                            "Checksum mismatch: provided = {}, calculated = {}",
+                            provided_checksum, calculated_checksum
+                        );
+                        break;
+                    }
+                    Err(e) => {
+                        println!("Failed to parse TBF header: {:?}", e);
+                        break;
+                    }
+                }
+                address += whole_len as u64;
+            }
+        }
+        Err(err) => println!("{}", err),
     }
+
+    Ok(())
 }
