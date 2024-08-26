7db3faad70fcb7290c92c7997206b4e6de9f8e08
Negrila Rares <129112753+NegrilaRares@users.noreply.github.com>
2024-08-26 14:22:50 +0300

    feat(info): support multiple footers (#26)

--- tockloader-lib/src/attributes/app_attributes.rs
+++ tockloader-lib/src/attributes/app_attributes.rs
@@ -6,19 +6,33 @@ use probe_rs::{Core, MemoryInterface};
 
 use tbf_parser::{
     self,
-    parse::{parse_tbf_header, parse_tbf_header_lengths},
-    types::TbfHeader,
+    parse::{parse_tbf_footer, parse_tbf_header, parse_tbf_header_lengths},
+    types::{TbfFooterV2Credentials, TbfHeader},
 };
 
 #[derive(Debug)]
 pub struct AppAttributes {
     pub tbf_header: TbfHeader,
+    pub tbf_footers: Vec<TbfFooter>,
+}
+
+#[derive(Debug)]
+pub struct TbfFooter {
+    pub credentials: TbfFooterV2Credentials,
+    pub size: u32,
+}
+
+impl TbfFooter {
+    fn new(credentials: TbfFooterV2Credentials, size: u32) -> TbfFooter {
+        TbfFooter { credentials, size }
+    }
 }
 
 impl AppAttributes {
-    pub(crate) fn new(header_data: TbfHeader) -> AppAttributes {
+    pub(crate) fn new(header_data: TbfHeader, footers_data: Vec<TbfFooter>) -> AppAttributes {
         AppAttributes {
             tbf_header: header_data,
+            tbf_footers: footers_data,
         }
     }
 
@@ -55,7 +69,40 @@ impl AppAttributes {
             let header: TbfHeader = parse_tbf_header(&header_data, tbf_version)
                 .unwrap_or_else(|e| panic!("Error found while getting tbf header data: {:?}", e));
 
-            let details: AppAttributes = AppAttributes::new(header);
+            let binary_end_offset = header.get_binary_end();
+
+            let mut footers: Vec<TbfFooter> = vec![];
+            let total_footers_size = total_size - binary_end_offset;
+            let mut footer_offset = binary_end_offset;
+            let mut footer_number = 0;
+
+            loop {
+                let mut appfooter = vec![
+                    0u8;
+                    (total_footers_size - (footer_offset - binary_end_offset))
+                        .try_into()
+                        .unwrap()
+                ];
+
+                // Do not ignore the result of the read operation
+                board_core
+                    .read(appaddr + footer_offset as u64, &mut appfooter)
+                    .unwrap();
+
+                let footer_info: (TbfFooterV2Credentials, u32) = parse_tbf_footer(&appfooter)
+                    .unwrap_or_else(|e| panic!("Paniced while obtaining footer data: {:?}", e));
+
+                footers.insert(footer_number, TbfFooter::new(footer_info.0, footer_info.1));
+
+                footer_number += 1;
+                footer_offset += footer_info.1 + 4;
+
+                if footer_offset == total_size {
+                    break;
+                }
+            }
+
+            let details: AppAttributes = AppAttributes::new(header, footers);
 
             apps_details.insert(apps_counter, details);
             apps_counter += 1;
