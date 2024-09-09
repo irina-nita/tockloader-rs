943e6f99c8fb3814a6527faad2f30afd75201827
Micu Ana <ana.micu@oxidos.io>
2024-09-09 17:42:52 +0300

    code refactoring

--- tockloader-lib/src/bootloader_serial.rs
+++ tockloader-lib/src/bootloader_serial.rs
@@ -6,8 +6,10 @@
 
 use crate::errors;
 use errors::TockloaderError;
-use std::{io::ErrorKind, time::Duration};
-use tokio_serial::{FlowControl, Parity, SerialPort, SerialPortInfo, SerialStream, StopBits};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio_serial::{SerialPort, SerialStream};
+use std::time::Duration;
+use bytes::BytesMut;
 
 // Tell the bootloader to reset its buffer to handle a new command
 pub const SYNC_MESSAGE: [u8; 3] = [0x00, 0xFC, 0x05];
@@ -15,6 +17,7 @@ pub const SYNC_MESSAGE: [u8; 3] = [0x00, 0xFC, 0x05];
 // "This was chosen as it is infrequent in .bin files" - immesys
 pub const ESCAPE_CHAR: u8 = 0xFC;
 
+#[allow(dead_code)]
 pub enum Command {
     // Commands from this tool to the bootloader
     CommandPing = 0x01,
@@ -41,6 +44,7 @@ pub enum Command {
     CommandSetStartAddress = 0x23,
 }
 
+#[derive(Clone, Debug)]
 pub enum Response {
     // Responses from the bootloader
     ResponseOverflow,
@@ -60,6 +64,7 @@ pub enum Response {
     ResponseCRCXF,
     ResponseInfo,
     ResponseChangeBaudFail,
+    BadResponse,
 }
 
 impl From<u8> for Response {
@@ -85,124 +90,165 @@ impl From<u8> for Response {
 
             // This error handling is temmporary
             //TODO(Micu Ana): Add error handling
-            _ => panic!("Invalid value for Response"),
+            _ => Response::BadResponse,
         }
     }
 }
 
-pub struct BootloaderSerial {
-    port: Option<SerialStream>,
+#[allow(dead_code)]
+pub async fn toggle_bootloader_entry_dtr_rts(port: &mut SerialStream) {
+    port
+        .write_data_terminal_ready(true)
+        .unwrap();
+    port
+        .write_request_to_send(true)
+        .unwrap();
+    tokio::time::sleep(Duration::from_millis(100)).await;
+    port
+        .write_data_terminal_ready(false)
+        .unwrap();
+    tokio::time::sleep(Duration::from_millis(500)).await;
+    port
+        .write_request_to_send(false)
+        .unwrap();
 }
 
-impl BootloaderSerial {
-    pub fn new(port: SerialPortInfo) -> Self {
-        // Open port and configure it with default settings using tokio_serial
-        let builder = tokio_serial::new(port.port_name, 115200);
-        match SerialStream::open(&builder) {
-            Ok(mut port) => {
-                port.set_parity(Parity::None).unwrap();
-                port.set_stop_bits(StopBits::One).unwrap();
-                port.set_flow_control(FlowControl::None).unwrap();
-                port.set_timeout(Duration::from_millis(500)).unwrap();
-                port.write_request_to_send(false).unwrap();
-                port.write_data_terminal_ready(false).unwrap();
-                return BootloaderSerial { port: Some(port) };
-            }
-            Err(_e) => {
-                //TODO(Micu Ana): Add error handling
-                return BootloaderSerial { port: None };
-            }
+#[allow(dead_code)]
+pub async fn ping_bootloader_and_wait_for_response(
+    port: &mut SerialStream,
+) -> Result<Response, TockloaderError> {
+    let ping_pkt = vec![ESCAPE_CHAR, Command::CommandPing as u8];
+
+    let mut ret = BytesMut::with_capacity(200);
+
+    for i in 0..30 {
+        println!("Iteration number {}", i);
+        let mut bytes_written = 0;
+        while bytes_written != ping_pkt.len() {
+            bytes_written += 
+                port
+                .write_buf(&mut &ping_pkt[bytes_written..])
+                .await?;
+            println!("Wrote {} bytes", bytes_written);
         }
-    }
-
-    pub fn issue_command(
-        &mut self,
-        command: Command,
-        mut message: Vec<u8>,
-        sync: bool,
-        response_len: usize,
-        response_code: Response,
-    ) -> Result<Response, TockloaderError> {
-        //Setup a command to send to the bootloader and handle the response
-
-        // Generate the message to send to the bootloader
-        for i in 0..message.len() {
-            if message[i] == ESCAPE_CHAR {
-                // Escaped by replacing all 0xFC with two consecutive 0xFC - tock bootloader readme
-                message.insert(i + 1, ESCAPE_CHAR);
-            }
+        let mut read_bytes = 0;
+        while read_bytes < 2 {
+            read_bytes += port.read_buf(&mut ret).await?;
         }
-        message.push(command as u8);
-
-        // If there should be a sync/reset message, prepend the outgoing message with it
-        if sync {
-            message.insert(0, SYNC_MESSAGE[0]);
-            message.insert(1, SYNC_MESSAGE[1]);
-            message.insert(2, SYNC_MESSAGE[2]);
-        }
-
-        // Write the command message
-        let _ = self.port.as_mut().unwrap().try_write(&message);
-
-        // Response has a two byte header, then response_len bytes
-        let _bytes_to_read = 2 + response_len;
-
-        // Loop to read in that number of bytes starting with the header
-        let mut ret: Vec<u8> = vec![0; 2];
-
-        // Receive the header bytes trying up to three times
-        for attempt in 0..3 {
-            // We assume that try_read() method doesn't read more than 2 bytes, based on the fact that
-            // if the return value of this method is [Ok(n)], then implementations must guarantee at
-            // least on Linux that 0 <= n <= ret.len()
-            // We check if we got two bytes, otherwise we try to read again
-            match self.port.as_mut().unwrap().try_read(&mut ret[0..2]) {
-                // If there are two values, we stop reading
-                Ok(2) => break,
-
-                // This error handling is temmporary
-                // TODO(Micu Ana): Add error handling
-                // We have to stop at this point since otherwise
-                // we loop waiting on data we will not get.
-                Ok(0) => {
-                    // We verify if it is the last attempt
-                    if attempt != 2 {
-                        continue;
-                    } else {
-                        // As it is no value we have nothing to return
-                        return Err(errors::TockloaderError::IOError(
-                            ErrorKind::InvalidData.into(),
-                        ));
-                    }
-                }
-                Ok(1) => {
-                    // We verify if it is the last attempt
-                    if attempt != 2 {
-                        continue;
-                    } else {
-                        // As it is only one value(response code) we want to return it
-                        return Ok(Response::from(ret[0]));
-                    }
-                }
-
-                // This error handling is temmporary
-                // TODO(Micu Ana): Add error handling
-                Err(e) => {
-                    return Err(errors::TockloaderError::IOError(e));
-                }
-
-                // We should never end up in this case, as we can't read more than 2 values
-                _ => {
-                    return Err(errors::TockloaderError::IOError(
-                        ErrorKind::InvalidData.into(),
-                    ));
-                }
-            }
-        }
-        if ret[0] != ESCAPE_CHAR {
-            //TODO(Micu Ana): Add error handling
+        println!("Read {} bytes", read_bytes);
+        if ret[1] == Response::ResponsePong as u8 {
             return Ok(Response::from(ret[1]));
         }
-        Ok(response_code)
     }
+    // TODO(Micu Ana): Add error handling
+    return Ok(Response::from(ret[1]));
+}
+
+#[allow(dead_code)]
+pub async fn issue_command(
+    port: &mut SerialStream,
+    command: Command,
+    mut message: Vec<u8>,
+    sync: bool,
+    response_len: usize,
+    response_code: Response,
+) -> Result<(Response, Vec<u8>), TockloaderError> {
+    // Setup a command to send to the bootloader and handle the response
+    // Generate the message to send to the bootloader
+    let mut i = 0;
+    while i < message.len() {
+        if message[i] == ESCAPE_CHAR {
+            // Escaped by replacing all 0xFC with two consecutive 0xFC - tock bootloader readme
+            message.insert(i + 1, ESCAPE_CHAR);
+            // Skip the inserted character
+            i += 2;
+        } else {
+            i += 1;
+        }
+    }
+    message.push(ESCAPE_CHAR);
+    message.push(command as u8);
+
+    // If there should be a sync/reset message, prepend the outgoing message with it
+    if sync {
+        message.insert(0, SYNC_MESSAGE[0]);
+        message.insert(1, SYNC_MESSAGE[1]);
+        message.insert(2, SYNC_MESSAGE[2]);
+    }
+
+    println!("Want to write {} bytes.", message.len());
+
+    // Write the command message
+    let mut bytes_written = 0;
+    while bytes_written != message.len() {
+        bytes_written += 
+            port
+            .write_buf(&mut &message[bytes_written..])
+            .await?;
+    }
+    println!("Wrote {} bytes", bytes_written);
+
+    tokio::time::sleep(Duration::from_millis(100)).await;
+    // Response has a two byte header, then response_len bytes
+    let bytes_to_read = 2 + response_len;
+    let mut ret = BytesMut::with_capacity(2);
+
+    // We are waiting for 2 bytes to be read
+    let mut read_bytes = 0;
+    while read_bytes < 2 {
+        read_bytes += port.read_buf(&mut ret).await?;
+    }
+    println!("Read {} bytes", read_bytes);
+    println!("{:?}", ret);
+
+    if ret[0] != ESCAPE_CHAR {
+        //TODO(Micu Ana): Add error handling
+        return Ok((Response::from(ret[1]), vec![]));
+    }
+
+    if ret[1] != response_code.clone() as u8 {
+        //TODO(Micu Ana): Add error handling
+        return Ok((Response::from(ret[1]), vec![]));
+    }
+
+    let mut new_data: Vec<u8> = Vec::new();
+
+    while bytes_to_read - ret.len() > 0 {
+        let value = port.read(&mut new_data).await?;
+
+        // Odd number of escape characters
+        // These can only come in pairs, so read another byte
+        let mut count = 0;
+        for i in 0..value {
+            if new_data[i] == ESCAPE_CHAR {
+                count += 1;
+            }
+        }
+        if count % 2 == 1 {
+            let byte: u8 = 0;
+            while byte == 0 {
+                port.read(&mut [byte]).await?;
+            }
+
+            new_data.push(byte);
+        }
+
+        // De-escape and add array of read in the bytes
+        for i in 0..(new_data.len() - 1) {
+            if new_data[i] == ESCAPE_CHAR {
+                if new_data[i + 1] == ESCAPE_CHAR {
+                    new_data.remove(i + 1);
+                }
+            }
+        }
+
+        ret.extend_from_slice(&new_data);
+    }
+
+    if ret.len() != (2 + response_len) {
+        // TODO(Micu Ana): Add error handling
+        return Ok((Response::from(ret[1]), vec![]));
+    }
+
+    Ok((Response::from(ret[1]), ret[2..].to_vec()))
 }
--- tockloader-lib/src/lib.rs
+++ tockloader-lib/src/lib.rs
@@ -2,159 +2,310 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // Copyright OXIDOS AUTOMOTIVE 2024.
 
+pub mod connection;
 pub mod attributes;
 mod bootloader_serial;
 mod errors;
-pub mod probe_session;
 pub mod tabs;
+
+use std::time::Duration;
 
 use attributes::app_attributes::AppAttributes;
 use attributes::general_attributes::GeneralAttributes;
 use attributes::system_attributes::SystemAttributes;
 
+use bootloader_serial::{issue_command, ping_bootloader_and_wait_for_response, toggle_bootloader_entry_dtr_rts, Command, Response};
+use connection::Connection;
+use probe_rs::flashing::DownloadOptions;
 use probe_rs::probe::DebugProbeInfo;
 use probe_rs::MemoryInterface;
-use probe_session::ProbeSession;
 
 use errors::TockloaderError;
 use tabs::tab::Tab;
 use tbf_parser::parse::parse_tbf_header_lengths;
+use tokio_serial::SerialPortInfo;
+
+pub fn list_debug_probes() -> Vec<DebugProbeInfo> {
+    probe_rs::probe::list::Lister::new().list_all()
+}
+
+pub fn list_serial_ports() -> Vec<SerialPortInfo> {
+    //TODO(Micu Ana): Add error handling
+    tokio_serial::available_ports().unwrap()
+}
 
 pub async fn list_probe(
-    choice: DebugProbeInfo,
-    chip: &str,
+    choice: Connection,
     core_index: &usize,
-) -> Vec<AppAttributes> {
-    let mut probe_session = ProbeSession::new(choice, chip);
-    let mut core = probe_session.get_core(*core_index);
+) -> Result<Vec<AppAttributes>, TockloaderError> {
 
-    let system_attributes = SystemAttributes::read_system_attributes(&mut core);
-
-    AppAttributes::read_apps_data(&mut core, system_attributes.appaddr.unwrap())
+    match choice {
+        Connection::ProbeRS(mut session) => {
+            let mut core = session.core(*core_index).unwrap();
+            let system_attributes = SystemAttributes::read_system_attributes(&mut core);
+            return Ok(AppAttributes::read_apps_data(&mut core, system_attributes.appaddr.unwrap()));
+        },
+        _ => {
+            // TODO(Micu Ana): Add error handling
+            return Err(TockloaderError::NoPortAvailable);
+        }
+    }
 }
 
 pub async fn info_probe(
-    choice: DebugProbeInfo,
-    chip: &str,
+    choice: Connection,
     core_index: &usize,
-) -> GeneralAttributes {
-    let mut probe_session = ProbeSession::new(choice, chip);
+) -> Result<GeneralAttributes, TockloaderError> {
 
-    let mut core = probe_session.get_core(*core_index);
-
-    let system_attributes = SystemAttributes::read_system_attributes(&mut core);
-
-    let apps_details = AppAttributes::read_apps_data(&mut core, system_attributes.appaddr.unwrap());
-
-    GeneralAttributes::new(system_attributes, apps_details)
+    match choice {
+        Connection::ProbeRS(mut session) => {
+            let mut core = session.core(*core_index).unwrap();
+            let system_attributes = SystemAttributes::read_system_attributes(&mut core);
+            let apps_details = AppAttributes::read_apps_data(&mut core, system_attributes.appaddr.unwrap());
+            Ok(GeneralAttributes::new(system_attributes, apps_details))
+        },
+        _ => {
+            // TODO(Micu Ana): Add error handling
+            return Err(TockloaderError::NoPortAvailable);
+        }
+    }
 }
 
-pub async fn install_app(
-    choice: DebugProbeInfo,
+pub async fn install_app_probe_rs(
+    choice: Connection,
     board: &String,
-    chip: &str,
     core_index: &usize,
     tab_file: Tab,
 ) -> Result<(), TockloaderError> {
-    let mut probe_session = ProbeSession::new(choice, chip);
-    let mut core = probe_session.get_core(*core_index);
 
-    // Verify if the specified app is compatible with board
-    match tab_file.is_compatible_with_board(board) {
-        Ok(value) => {
-            if value {
-                println!("Specified tab is compatible with board.");
+    match choice {
+        Connection::ProbeRS(mut session) => {
+
+            // Verify if the specified app is compatible with board
+            // TODO(Micu Ana): Replace the prints with log messages
+            match tab_file.is_compatible_with_board(board) {
+                Ok(value) => {
+                    if value {
+                        println!("Specified tab is compatible with board.");
+                    } else {
+                        println!("Specified tab is not compatible with board.");
+                    }
+                }
+                Err(e) => println!("Something went wrong: {:?}", e),
+            }
+
+            // Get core - if not specified, by default is 0
+            // TODO (Micu Ana): Add error handling
+            let mut core = session.core(*core_index).unwrap();
+
+            // Get board data
+            let system_attributes = SystemAttributes::read_system_attributes(&mut core);
+            let kernel_version = system_attributes.kernel_version.unwrap();
+
+            // Verify if the specified app is compatible with kernel version
+            // TODO(Micu Ana): Replace the prints with log messages
+            match tab_file.is_compatible_with_kernel_verison(kernel_version as f32) {
+                Ok(value) => {
+                    if value {
+                        println!("Specified tab is compatible with your kernel version.");
+                    } else {
+                        println!("Specified tab is not compatible with your kernel version.");
+                    }
+                }
+                Err(e) => println!("Something went wrong: {:?}", e),
+            }
+
+            // Get the address from which we start writing the new app
+            // TODO: change appaddr to 32 bit
+            // TODO for the future: support 64 bit arhitecture
+            let mut address: u64 = system_attributes.appaddr.unwrap();
+
+            // Loop to check if there are another apps installed
+            loop {
+                // Read a block of 200 8-bit words
+                let mut buff = vec![0u8; 200];
+                match core.read(address, &mut buff) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error reading memory: {:?}", e);
+                        break;
+                    }
+                }
+                let (_ver, _header_len, whole_len) =
+                    match parse_tbf_header_lengths(&buff[0..8].try_into().unwrap()) {
+                        Ok((ver, header_len, whole_len)) if header_len != 0 => (ver, header_len, whole_len),
+                        _ => break, // No more apps
+                    };
+        
+                address += whole_len as u64;
+            }
+
+            // Create app object
+            let app = tab_file
+                .extract_app(system_attributes.arch.clone())
+                .unwrap();
+            let header_binary = tab_file.extract_header_binary(system_attributes.arch.clone());
+            dbg!(header_binary.len());
+            let footer_binary = tab_file.extract_footer_binary(system_attributes.arch.clone());
+            dbg!(footer_binary.len());
+            let mut binary = app.get_app_binary();
+            dbg!(binary.len());
+            
+            // Concatenate all binaries (header + app + footer)
+            let mut full_binary = Vec::new();
+            for i in header_binary {
+                full_binary.push(i);
+            }
+
+            for i in binary {
+                full_binary.push(i);
+            }
+            for i in footer_binary {
+                full_binary.push(i);
+            }
+
+            binary = full_binary;
+
+            let size = binary.len() as u64;
+
+            // Make sure the app is aligned to a multiple of its size
+            let multiple = address / size;
+
+            let (new_address, _gap_size) = if multiple * size != address {
+                let new_address = ((address + size) / size) * size;
+                let gap_size = new_address - address;
+                (new_address, gap_size)
             } else {
-                println!("Specified tab is not compatible with board.");
-            }
-        }
-        Err(e) => println!("Something went wrong: {:?}", e),
-    }
-
-    // Get board data
-    let system_attributes = SystemAttributes::read_system_attributes(&mut core);
-    let kernel_version = system_attributes.kernel_version.unwrap();
-
-    // Verify if the specified app is compatible with kernel version
-    match tab_file.is_compatible_with_kernel_verison(kernel_version as f32) {
-        Ok(value) => {
-            if value {
-                println!("Specified tab is compatible with your kernel version.");
-            } else {
-                println!("Specified tab is not compatible with your kernel version.");
-            }
-        }
-        Err(e) => println!("Something went wrong: {:?}", e),
-    }
-
-    // Get the address from which we start writing the new app
-    let mut address: u64 = system_attributes.appaddr.unwrap();
-
-    loop {
-        // Read a block of 200 8-bit words
-        let mut buff = vec![0u8; 200];
-        match core.read(address, &mut buff) {
-            Ok(_) => {}
-            Err(e) => {
-                println!("Error reading memory: {:?}", e);
-                break;
-            }
-        }
-        let (_ver, _header_len, whole_len) =
-            match parse_tbf_header_lengths(&buff[0..8].try_into().unwrap()) {
-                Ok((ver, header_len, whole_len)) if header_len != 0 => (ver, header_len, whole_len),
-                _ => break, // No more apps
+                (address, 0)
             };
 
-        address += whole_len as u64;
-    }
+            // No more need of core
+            drop(core);
 
-    // Create app object
-    let app = tab_file.extract_app(system_attributes.arch).unwrap();
-    let size = app.get_size() as u64;
+            // Make sure the binary is a multiple of the page size by padding 0xFFs
+            // TODO(Micu Ana): check if the page-size differs
+            let page_size = 512;
+            let needs_padding = binary.len() % page_size != 0;
 
-    // Make sure the app is aligned to a multiple of its size
-    let multiple = address / size;
-    let (new_address, gap_size) = if multiple * size != address {
-        let new_address = ((address + size) / size) * size;
-        let gap_size = new_address - address;
-        (new_address, gap_size)
-    } else {
-        (address, 0)
-    };
+            dbg!(needs_padding);
 
-    // Make sure the binary is a multiple of the page size by padding 0xFFs
-    // TODO(Micu Ana): check if the page-size differs
-    let page_size = 512;
-    let binary = app.get_app_binary();
-    let binary_len = binary.len();
-    let needs_padding = binary_len % page_size != 0;
+            if needs_padding {
+                let remaining = page_size - (binary.len() % page_size);
+                dbg!(remaining);
+                for _i in 0..remaining {
+                    binary.push(0xFF);
+                }
+            }
 
-    let mut app = app;
-    if gap_size > 0 {
-        app.set_padding(gap_size);
-    }
+            let binary_len = binary.len();
 
-    if needs_padding {
-        let remaining = page_size - (binary_len % page_size);
-        app.add_padding_to_app_binary(remaining);
-    }
+            // Get indices of pages that have valid data to write
+            let mut valid_pages: Vec<u8> = Vec::new();
+            for i in 0..(binary_len / page_size) {
+                for b in binary[(i * page_size)..((i + 1) * page_size)].to_vec() {
+                    if b != 0 {
+                        valid_pages.push(i.try_into().unwrap());
+                        break;
+                    }
+                }
+            }
 
-    // Get indices of pages that have valid data to write
-    let valid_pages: Vec<u8> = app.get_valid_pages(binary_len, page_size);
+            // If there are no pages valid, all pages would have been removed, so we write them all
+            if valid_pages.len() == 0 {
+                for i in 0..(binary_len / page_size) {
+                    valid_pages.push(i.try_into().unwrap());
+                }
+            }
 
-    for i in valid_pages {
-        // Create the packet that we send to the bootloader
-        // First four bytes are the address of the page
-        let mut pkt = (new_address + page_size as u64).to_le_bytes().to_vec();
+            // Include a blank page (if exists) after the end of a valid page. There might be a usable 0 on the next page
+            let mut ending_pages: Vec<u8> = Vec::new();
+            for &i in &valid_pages {
+                let mut iter = valid_pages.iter();
+                if iter.find(|&&x| x == (i + 1)).is_none() && (i + 1) < (binary_len / page_size) as u8 {
+                    ending_pages.push(i + 1);
+                }
+            }
 
-        // Then the bytes that go into the page
-        for b in binary[(i as usize * page_size)..((i + 1) as usize * page_size)].to_vec() {
-            pkt.push(b);
+            for i in ending_pages {
+                valid_pages.push(i);
+            }
+
+            for i in valid_pages {
+                println!("Writing page number {}", i);
+                // Create the packet that we send to the bootloader
+                // First four bytes are the address of the page
+                let mut pkt = Vec::new();
+                        
+                // Then the bytes that go into the page
+                for b in binary[(i as usize * page_size)..((i + 1) as usize * page_size)].to_vec() {
+                    pkt.push(b);
+                }
+                let mut loader = session.target().flash_loader();
+
+                loader.add_data(((new_address as u32 + (i as usize * page_size) as u32)).into(), &pkt).unwrap();
+
+                let mut options = DownloadOptions::default();
+                options.keep_unwritten_bytes = true;
+
+                // Finally, the data can be programmed
+                loader.commit(&mut session, options).unwrap();
+            }
         }
-
-        // TODO(Micu Ana): Write to bootloader
+        
+        _ => {
+            // TODO(Micu Ana): Add error handling
+            return Err(TockloaderError::NoPortAvailable);
+        }
     }
+    Ok(())
+}
 
+pub async fn install_app_serial(
+    choice: Connection,
+    board: &String,
+    tab_file: Tab,
+) -> Result<(), TockloaderError> {
+
+    match choice {
+        Connection::Serial(mut port) => {
+            // Verify if the specified app is compatible with board
+            match tab_file.is_compatible_with_board(board) {
+                Ok(value) => {
+                    if value {
+                        println!("Specified tab is compatible with board.");
+                    } else {
+                        println!("Specified tab is not compatible with board.");
+                    }
+                }
+                Err(e) => println!("Something went wrong: {:?}", e),
+            }
+
+            let response = ping_bootloader_and_wait_for_response(&mut port).await?;
+
+            if response as u8 != Response::ResponsePong as u8 {
+                toggle_bootloader_entry_dtr_rts(&mut port).await;
+                tokio::time::sleep(Duration::from_millis(100)).await;
+                let _ = ping_bootloader_and_wait_for_response(&mut port).await?;
+            }
+
+            let (_response, message) = issue_command(
+                        &mut port,
+                        Command::CommandInfo,
+                        vec![],
+                        true,
+                        0,
+                        Response::ResponseInfo,
+                    )
+                    .await
+                    .unwrap();
+
+            dbg!(message);
+                    
+        }
+        _ => {
+            // TODO(Micu Ana): Add error handling
+            return Err(TockloaderError::NoPortAvailable);
+        }
+    }
     Ok(())
 }
