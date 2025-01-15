059d8dc966b5b75df3e6e188f525099fa965a89c
Irina Nita <irinaanita18@gmail.com>
2025-01-15 16:37:56 +0200

    Removed async for install app

--- tockloader-lib/src/connection.rs
+++ tockloader-lib/src/connection.rs
@@ -1,52 +1,22 @@
-use std::{sync::Arc, time::Duration};
+use std::sync::Arc;
 
 use parking_lot::FairMutex;
-use probe_rs::{probe::DebugProbeInfo, Permissions, Session};
-use tokio_serial::{FlowControl, Parity, SerialPort, SerialStream, StopBits};
+use probe_rs::{probe::DebugProbeInfo, Permissions};
 
 use crate::errors::TockloaderError;
 
 pub enum ConnectionInfo {
-    SerialInfo(String),
     ProbeInfo(DebugProbeInfo),
-}
-
-impl From<String> for ConnectionInfo {
-    fn from(value: String) -> Self {
-        ConnectionInfo::SerialInfo(value)
-    }
 }
 
 pub enum Connection {
     // A probe-rs session can be shared between threads.
-    ProbeRS(Arc<FairMutex<Session>>),
-    Serial(SerialStream),
+    ProbeRS(Arc<FairMutex<probe_rs::Session>>),
 }
 
 impl Connection {
     pub fn open(info: ConnectionInfo, chip: Option<String>) -> Result<Connection, TockloaderError> {
         match info {
-            ConnectionInfo::SerialInfo(serial_info) => {
-                let builder = tokio_serial::new(serial_info, 115200);
-                match SerialStream::open(&builder) {
-                    Ok(mut port) => {
-                        port.set_parity(Parity::None)
-                            .map_err(TockloaderError::SerialInitializationError)?;
-                        port.set_stop_bits(StopBits::One)
-                            .map_err(TockloaderError::SerialInitializationError)?;
-                        port.set_flow_control(FlowControl::None)
-                            .map_err(TockloaderError::SerialInitializationError)?;
-                        port.set_timeout(Duration::from_millis(500))
-                            .map_err(TockloaderError::SerialInitializationError)?;
-                        port.write_request_to_send(false)
-                            .map_err(TockloaderError::SerialInitializationError)?;
-                        port.write_data_terminal_ready(false)
-                            .map_err(TockloaderError::SerialInitializationError)?;
-                        Ok(Connection::Serial(port))
-                    }
-                    Err(e) => Err(TockloaderError::SerialInitializationError(e)),
-                }
-            }
             ConnectionInfo::ProbeInfo(probe_info) => {
                 let probe = probe_info
                     .open()
