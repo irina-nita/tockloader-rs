099c8b590149b9ef95342a938d42a40235786459
Irina Nita <irinaanita18@gmail.com>
2025-01-15 17:20:27 +0200

    Add tbf loading

--- tockloader-lib/src/lib.rs
+++ tockloader-lib/src/lib.rs
@@ -9,6 +9,9 @@ pub mod errors;
 pub mod tabs;
 
 use attributes::system_attributes::SystemAttributes;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 use std::sync::Arc;
 
 use connection::Connection;
@@ -110,6 +113,166 @@ pub fn install_app(
                 ))?;
 
             let mut binary = tab_file.extract_binary(&arch.clone())?; // use the system_attributes arch or the provided one?
+
+            let size = binary.len() as u64;
+
+            // Make sure the app is aligned to a multiple of its size
+            let multiple = address / size;
+
+            let (new_address, _gap_size) = if multiple * size != address {
+                let new_address = ((address + size) / size) * size;
+                let gap_size = new_address - address;
+                (new_address, gap_size)
+            } else {
+                (address, 0)
+            };
+
+            // No more need of core
+            drop(core);
+
+            // Make sure the binary is a multiple of the page size by padding 0xFFs
+            // TODO(Micu Ana): check if the page-size differs
+            let page_size = 512;
+            let needs_padding = binary.len() % page_size != 0;
+
+            if needs_padding {
+                let remaining = page_size - (binary.len() % page_size);
+                dbg!(remaining);
+                for _i in 0..remaining {
+                    binary.push(0xFF);
+                }
+            }
+
+            // Get indices of pages that have valid data to write
+            let mut valid_pages: Vec<u8> = Vec::new();
+            for i in 0..(size as usize / page_size) {
+                for b in binary[(i * page_size)..((i + 1) * page_size)]
+                    .iter()
+                    .copied()
+                {
+                    if b != 0 {
+                        valid_pages.push(i.try_into().unwrap());
+                        break;
+                    }
+                }
+            }
+
+            // If there are no pages valid, all pages would have been removed, so we write them all
+            if valid_pages.is_empty() {
+                for i in 0..(size as usize / page_size) {
+                    valid_pages.push(i.try_into().unwrap());
+                }
+            }
+
+            // Include a blank page (if exists) after the end of a valid page. There might be a usable 0 on the next page
+            let mut ending_pages: Vec<u8> = Vec::new();
+            for &i in &valid_pages {
+                let mut iter = valid_pages.iter();
+                if !iter.any(|&x| x == (i + 1)) && (i + 1) < (size as usize / page_size) as u8 {
+                    ending_pages.push(i + 1);
+                }
+            }
+
+            for i in ending_pages {
+                valid_pages.push(i);
+            }
+
+            for i in valid_pages {
+                println!("Writing page number {}", i);
+                // Create the packet that we send to the bootloader
+                // First four bytes are the address of the page
+                let mut pkt = Vec::new();
+
+                // Then the bytes that go into the page
+                for b in binary[(i as usize * page_size)..((i + 1) as usize * page_size)]
+                    .iter()
+                    .copied()
+                {
+                    pkt.push(b);
+                }
+                let mut loader = session.target().flash_loader();
+
+                loader
+                    .add_data(
+                        (new_address as u32 + (i as usize * page_size) as u32).into(),
+                        &pkt,
+                    )
+                    .map_err(TockloaderError::ProbeRsWriteError)?;
+
+                let mut options = DownloadOptions::default();
+                options.keep_unwritten_bytes = true;
+
+                // Finally, the data can be programmed
+                loader
+                    .commit(&mut session, options)
+                    .map_err(TockloaderError::ProbeRsWriteError)?;
+            }
+
+            Ok(())
+        }
+    }
+}
+
+pub fn install_tbf(
+    choice: Connection,
+    core_index: Option<&usize>,
+    tbf_file: impl AsRef<Path>,
+) -> Result<(), TockloaderError> {
+    match choice {
+        Connection::ProbeRS(session) => {
+            let session = Arc::clone(&session);
+            let mut session = session.lock();
+            // Get core - if not specified, by default is 0
+            let mut core = session
+                .core(*core_index.unwrap())
+                .map_err(|e| TockloaderError::CoreAccessError(*core_index.unwrap(), e))?;
+            // Get board data
+            let system_attributes = SystemAttributes::read_system_attributes_probe(&mut core)?;
+
+            let board = system_attributes
+                .board
+                .ok_or(TockloaderError::MisconfiguredBoard(
+                    "No board name found.".to_owned(),
+                ))?;
+            let kernel_version =
+                system_attributes
+                    .kernel_version
+                    .ok_or(TockloaderError::MisconfiguredBoard(
+                        "No kernel version found.".to_owned(),
+                    ))?;
+ 
+            // Get the address from which we start writing the new app
+            // TODO: change appaddr to 32 bit
+            // TODO for the future: support 64 bit arhitecture
+            let mut address =
+                system_attributes
+                    .appaddr
+                    .ok_or(TockloaderError::MisconfiguredBoard(
+                        "No start address found.".to_owned(),
+                    ))?;
+
+            // Loop to check if there are another apps installed
+            loop {
+                // Read a block of 200 8-bit words
+                let mut buff = vec![0u8; 200];
+                core.read(address, &mut buff)
+                    .map_err(TockloaderError::ProbeRsReadError)?;
+
+                let (_ver, _header_len, whole_len) = match parse_tbf_header_lengths(
+                    &buff[0..8]
+                        .try_into()
+                        .expect("Buffer length must be at least 8 bytes long."),
+                ) {
+                    Ok((ver, header_len, whole_len)) if header_len != 0 => {
+                        (ver, header_len, whole_len)
+                    }
+                    _ => break, // No more apps
+                };
+                address += whole_len as u64;
+            }
+
+            let mut binary = vec![];
+            File::open(tbf_file).unwrap().read_to_end(&mut binary).unwrap(); // use the system_attributes arch or the provided one?
 
             let size = binary.len() as u64;
 
